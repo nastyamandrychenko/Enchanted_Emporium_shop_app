fcdc6ebcdd9cf1ecd82f14158a1e0b866255e170
nastyamandrychenko <nastyamarichenko@gmail.com>
2024-11-07 15:31:41 +0100

    added fragment classes

---
+++ app/src/main/java/hu/bme/aut/qrvhfq/EnchantedEmporium/fragments/log_reg/AccountOptionsFragm.kt
@@ -0,0 +1,7 @@
+package hu.bme.aut.qrvhfq.EnchantedEmporium.fragments.log_reg
+
+import androidx.fragment.app.Fragment
+import hu.bme.aut.qrvhfq.myapplication.R
+
+class AccountOptionsFragm : Fragment(R.layout.account_fragm){
+}
---
+++ app/src/main/java/hu/bme/aut/qrvhfq/EnchantedEmporium/fragments/log_reg/IntroFragm.kt
@@ -0,0 +1,8 @@
+package hu.bme.aut.qrvhfq.EnchantedEmporium.fragments.log_reg
+
+import androidx.fragment.app.Fragment
+import hu.bme.aut.qrvhfq.myapplication.R
+
+class IntroFragm : Fragment(R.layout.intro_fragm) {
+
+}
