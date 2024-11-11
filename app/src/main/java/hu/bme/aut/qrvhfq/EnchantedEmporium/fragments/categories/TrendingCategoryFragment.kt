7f6761c0231c05e3f0bc1ebe0bb51252b7403505
nastyamandrychenko <nastyamarichenko@gmail.com>
2024-11-11 17:27:07 +0100

    added recyclerview

--- app/src/main/java/hu/bme/aut/qrvhfq/EnchantedEmporium/fragments/categories/TrendingCategoryFragment.kt
+++ app/src/main/java/hu/bme/aut/qrvhfq/EnchantedEmporium/fragments/categories/TrendingCategoryFragment.kt
@@ -3,6 +3,6 @@ package hu.bme.aut.qrvhfq.EnchantedEmporium.fragments.categories
 import androidx.fragment.app.Fragment
 import hu.bme.aut.qrvhfq.myapplication.R
 
-class TrendingCategoryFragment : Fragment(R.layout.fragment_category) {
+class TrendingCategoryFragment : Fragment(R.layout.fragment_trending_category) {
 
 }
--- app/src/main/java/hu/bme/aut/qrvhfq/EnchantedEmporium/fragments/categories/TrendingNowCategoryFragment.kt
+++ app/src/main/java/hu/bme/aut/qrvhfq/EnchantedEmporium/fragments/categories/TrendingNowCategoryFragment.kt
@@ -3,5 +3,5 @@ package hu.bme.aut.qrvhfq.EnchantedEmporium.fragments.categories
 import androidx.fragment.app.Fragment
 import hu.bme.aut.qrvhfq.myapplication.R
 
-open class TrendingNowCategoryFragment : Fragment(R.layout.fragment_trending_now){
+open class TrendingNowCategoryFragment : Fragment(R.layout.fragment_trending_category){
 }
