929c8c174fa05829b46d5d3b529ff5fd7b90acb6
nastyamandrychenko <nastyamarichenko@gmail.com>
2024-11-30 17:20:39 +0100

    added address fragment+layout

---
+++ app/src/main/java/hu/bme/aut/qrvhfq/EnchantedEmporium/fragments/shopping/AdressFragm.kt
@@ -0,0 +1,7 @@
+package hu.bme.aut.qrvhfq.EnchantedEmporium.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import hu.bme.aut.qrvhfq.myapplication.R
+
+class AdressFragm : Fragment(R.layout.adress_fragment){
+}
